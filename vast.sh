9be2e4a38c9a2ec42ef034f1517693dd4b2c3c70
doog31fb <75453668+doog31fb@users.noreply.github.com>
2023-10-23 13:55:41 -0400

    vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x978381cDF8a72D7ad919e01652D31C3735ea3b69/g' config.conf
 sleep 5
 echo "---------------------------"
 
